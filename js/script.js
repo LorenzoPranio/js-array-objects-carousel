b46fc20b29d57b91db535df6be4c518ce5f7a0df
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-11 17:16:25 +0200

    [UPD]Added btn-dx click function in script.js

--- js/script.js
+++ js/script.js
@@ -25,12 +25,9 @@ const images = [
 //Rimuovo le immagini dal file HTML e lascio solo il contenitore vuoto
 const container = document.querySelector('.container');
 
-const btn_sx = document.querySelector('.btn-sx');
-const btn_dx = document.querySelector('.btn-dx');
-
 images.forEach((el) => {
 container.innerHTML += `<div class="card d-none">
-<img src="${el.image}" alt="Miles Morales">
+<img src="${el.image}" alt="">
 <div class="text">
     <h2>${el.title}</h2>
     <p>${el.text}</p>
@@ -41,15 +38,25 @@ container.innerHTML += `<div class="card d-none">
 //Creo le frecce che al click permettono all'utente di navigare tra le immagini
 const card = document.querySelectorAll('.card');
 
+//Recuepro le costanti dei bottoni
+const btn_sx = document.querySelector('.btn-sx');
+const btn_dx = document.querySelector('.btn-dx');
+
 //Creo la variabile per l'elelemento attivo
 let active = 0;
 card[active].classList.remove('d-none');
 
+
 btn_dx.addEventListener('click', function(){
-    card[active].classList.add('.d-none');
-    active++;
-    card[active].classList.remove('.d-none');
-})
+    card[active].classList.add('d-none');
+    if(active == images.length - 1){
+        active = 0;
+    }
+    else{
+       active++; 
+    }
+    card[active].classList.remove('d-none');
+});
 
 
 
